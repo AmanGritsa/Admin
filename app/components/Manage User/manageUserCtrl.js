ba3c2be5a728893d71c85defc4f013a333f678bd
Mobulous85 <aman.verma@mobulous.com>
2018-05-07 15:56:50 +0530

    add messages

--- app/components/Manage User/manageUserCtrl.js
+++ app/components/Manage User/manageUserCtrl.js
@@ -1,6 +1,6 @@
 cyndyApp.controller('manageUserCtrl', function ($scope, $state, apiCall) {
     var token = localStorage.getItem('token');
-   $scope.myLoader = true;
+    $scope.myLoader = true;
     if (token == 'null') {
         $state.go('login');
     }
@@ -19,6 +19,41 @@ cyndyApp.controller('manageUserCtrl', function ($scope, $state, apiCall) {
                 alert(dataResponse.data.message);
             }
         });
+
+        $scope.sendMessage = function (user) {
+            swal({
+                title: "Success thru Style",
+                text: "<textarea id='text' placeholder='Write a message....' style='height: 90px;width: 95%'></textarea>",
+                html: true,
+                showCancelButton: true,
+                closeOnConfirm: false,
+            }, function (inputValue) {
+                var val = document.getElementById('text').value;
+                if (inputValue === false) return false;
+                if (val === "") {
+                    swal.showInputError("You need to write something!");
+                    return false
+                }
+
+                var json = {
+                    userType: userType,
+                    email: user.email,
+                    message: val
+                };
+                apiCall.getData('POST', 'sendMessageToUser', json, token).then(function (dataResponse) {
+                    if (dataResponse.data.status == 200) {
+                        swal("Sent!", "Your message has been sent to " + user.userName, "success");
+                    }
+                    else {
+                        alert(dataResponse.data.message);
+                    }
+                });
+
+            });
+
+
+        }
+
     }
 
 })
